a69ff944682590b7826ec627ceef4aa3747b279f
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2022-09-26 17:16:19 +0300

    Created basic app with image and appbar.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.newscientist.com/wp-content/uploads/2022/04/06095721/SEI_97255809.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
